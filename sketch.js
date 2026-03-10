f2d62140770674f9fa048752c8e7b7e0fa063e9c
Cactuss <martiparra@ieaoriolmartorell.cat>
2026-03-10 12:53:30 +0100

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById('wishlistBtn');
+
+btn.addEventListener('click', () => {
+  btn.classList.add('clicked');
+  btn.disabled = true; // evita clicks repetidos
+  register()
+});
+
+function register(){
+  storeItem("wishlist", true)
+}
+
+function setup(){
+  if(getItem("wishlist")){
+    btn.classList.add('clicked');
+    btn.disabled = true;
+  }
+}
